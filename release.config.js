f1c1337518ab4484fe0f6e56e4b92777edb7933b
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2024-01-27 13:21:55 +0000

    chore: latest changes

--- release.config.js
+++ release.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-    branches: [
-      "main", // string
-      { name: "beta", prerelease: true }, // object with name property
-      // You can add more branches as needed
-    ],
-  };
-  
+	branches: [
+		'main', // string
+		{ name: 'beta', prerelease: true }, // object with name property
+		// You can add more branches as needed
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		'@semantic-release/github',
+		[
+			'@semantic-release/changelog',
+			{
+				changelogFile: 'CHANGELOG.md',
+			},
+		],
+	],
+};
