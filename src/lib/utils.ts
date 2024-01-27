7971773f8c8a75d02e3329a9cbe9537c6d7309ae
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2024-01-27 15:58:27 +0000

    fix: add missing dependencie

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -130,7 +130,7 @@ export async function installEPAForTS(pth: string) {
 
 	logger.info('[NO INSTALL] Adding packages');
 	shell.exec(
-		'npm install --no-save-dev eslint eslint-config-prettier @typescript-eslint/eslint-plugin prettier eslint-config-airbnb-typescript @typescript-eslint/parser'
+		'npm install --no-save-dev eslint eslint-config-prettier @typescript-eslint/eslint-plugin prettier eslint-config-airbnb-typescript airbnb-typescript/base @typescript-eslint/parser'
 	);
 
 	shell.exec('npx install-peerdeps --dev eslint-config-airbnb-base');
