b06d45842ee388a38e0f282147cb25359fe95557
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2024-01-27 13:29:13 +0000

    chore: add missing dependencies

--- release.config.js
+++ release.config.js
@@ -7,13 +7,13 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
-		'@semantic-release/npm',
-		'@semantic-release/github',
 		[
 			'@semantic-release/changelog',
 			{
 				changelogFile: 'docs/CHANGELOG.md',
 			},
 		],
+		'@semantic-release/npm',
+		'@semantic-release/github',
 	],
 };
