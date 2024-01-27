e0f7dd069dba6efcc7c38508926fb92be23ee82c
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2024-01-27 12:38:35 +0000

    chore: use semantic-versioning

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: [
+      "main", // string
+      { name: "beta", prerelease: true }, // object with name property
+      // You can add more branches as needed
+    ],
+  };
+  
