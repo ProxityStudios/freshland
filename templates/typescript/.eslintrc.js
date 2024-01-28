43607bfda665c985d7fe021674271a7bcf6d45b4
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2024-01-28 11:27:16 +0000

    style: eslint prettier fixes

--- templates/typescript/.eslintrc.js
+++ templates/typescript/.eslintrc.js
@@ -3,55 +3,53 @@ module.exports = {
   root: true,
   plugins: ['@typescript-eslint'],
   extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:import/typescript',
-    'plugin:prettier/recommended',
+     'airbnb-base',
+     'airbnb-typescript/base',
+     'plugin:@typescript-eslint/recommended-type-checked',
+     'plugin:@typescript-eslint/stylistic-type-checked',
+     'plugin:import/typescript',
+     'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: true,
-		tsconfigRootDir: __dirname,
-	},
+  parserOptions: {
+     project: true,
+     tsconfigRootDir: __dirname,
+  },
   rules: {
-    'no-prototype-builtins': 'off',
-		'import/prefer-default-export': 'off',
-		'import/no-default-export': 'off',
-		'@typescript-eslint/no-unused-vars': 'warn',
-		'@typescript-eslint/no-explicit-any': 'warn',
-		'@typescript-eslint/no-unsafe-assignment': 'warn',
-		'@typescript-eslint/no-unsafe-member-access': 'warn',
-		'no-use-before-define': [
-			'error',
-			{ functions: false, classes: true, variables: true },
-		],
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-use-before-define': [
-			'error',
-			{ functions: false, classes: true, variables: true, typedefs: true },
-		],
-    "prettier/prettier": [
-      "error",
-      {},
-      {
-        "usePrettierrc": true
-      }
-    ],
-    'class-methods-use-this': 'off',
+     'no-prototype-builtins': 'off',
+     'import/prefer-default-export': 'off',
+     'import/no-default-export': 'off',
+     '@typescript-eslint/no-unused-vars': 'warn',
+     '@typescript-eslint/no-explicit-any': 'warn',
+     '@typescript-eslint/no-unsafe-assignment': 'warn',
+     '@typescript-eslint/no-unsafe-member-access': 'warn',
+     'no-use-before-define': [
+        'error',
+        { functions: false, classes: true, variables: true },
+     ],
+     '@typescript-eslint/explicit-function-return-type': 'off',
+     '@typescript-eslint/no-use-before-define': [
+        'error',
+        {
+           functions: false,
+           classes: true,
+           variables: true,
+           typedefs: true,
+        },
+     ],
+     'class-methods-use-this': 'off',
   },
   env: {
-    node: true,
+     node: true,
   },
   overrides: [
-    {
-      files: ['*.js'],
-      extends: ['plugin:@typescript-eslint/disable-type-checked'],
-      rules: {
-        '@typescript-eslint/internal/no-poorly-typed-ts-props': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-      },
-    },
-  ]
+     {
+        files: ['*.js'],
+        extends: ['plugin:@typescript-eslint/disable-type-checked'],
+        rules: {
+           '@typescript-eslint/internal/no-poorly-typed-ts-props': 'off',
+           '@typescript-eslint/explicit-function-return-type': 'off',
+        },
+     },
+  ],
 };
