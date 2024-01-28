64c8e6756241e726c63ff94157f8c0e245edf706
Alexinhans <Alexinhans@users.noreply.github.com>
2024-01-28 20:27:23 +0000

    Automated push

--- dist/lib/dir.js
+++ dist/lib/dir.js
@@ -5,5 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.rootDir = void 0;
 const node_path_1 = __importDefault(require("node:path"));
-// eslint-disable-next-line unicorn/prefer-module
 exports.rootDir = node_path_1.default.resolve(__dirname, '..', '..');
--- dist/lib/utils.d.ts
+++ dist/lib/utils.d.ts
@@ -1,8 +1,8 @@
 import { PackageManager } from '../types';
 export declare function cloneGithubRepo(repo: string, destination: string): void;
 export declare function deleteAndInitGit(pth: string): void;
-export declare function updatePackageJSON(projectName: string, pth: string): void;
-export declare function installDeps(packageManager: PackageManager, projectName: string, pth: string): void;
+export declare function updatePackageJSON(packageName: string, packageVersion: string, pth: string): void;
+export declare function installDeps(packageManager: PackageManager, pth: string): void;
 export declare function initEPAForTS(pth: string): Promise<void>;
-export declare function initEPAForJS(): void;
+export declare function initEPAForJS(pth: string): void;
 //# sourceMappingURL=utils.d.ts.map
