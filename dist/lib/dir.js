90e8b5f185c9187c5c12157eb04ad4539dfd66c9
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2024-01-29 10:43:41 -0800

    fix: testing

---
+++ dist/lib/dir.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rootDir = void 0;
+const node_path_1 = __importDefault(require("node:path"));
+exports.rootDir = node_path_1.default.resolve(__dirname, '..', '..');
