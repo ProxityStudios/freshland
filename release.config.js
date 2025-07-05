5cb71e89d7bd0f69b6ed1f432ff8172aaaa6cdc2
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2025-07-05 11:51:00 +0000

    fix: trigger workflow

--- release.config.js
+++ release.config.js
@@ -19,25 +19,9 @@ module.exports = {
       },
     ],
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: './docs/CHANGELOG.md',
-      },
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        pkgRoot: './package',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['docs/', './package.json', './package-lock.json'],
-        message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
     '@semantic-release/github',
   ],
 };
