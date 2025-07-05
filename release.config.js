6760b4c689b1b554bcd70d295878dbb95b4f747c
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2025-07-05 12:15:55 +0000

    fix: trigger workflow

--- release.config.js
+++ release.config.js
@@ -4,24 +4,22 @@
 module.exports = {
   branches: ['main', { name: 'beta', prerelease: true }],
   plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
-      '@semantic-release/commit-analyzer',
+      '@semantic-release/changelog',
       {
-        preset: 'angular',
-        releaseRules: [
-          { type: 'docs', scope: 'README', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
-        ],
-        parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-        },
+        changelogFile: './docs/CHANGELOG.md',
       },
     ],
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['/docs', './package.json'],
+        message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+      },
+    ],
     '@semantic-release/github',
   ],
 };
