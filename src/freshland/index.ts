74ff819bac4a894af601d34d25847c425dc6d318
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2025-07-05 12:58:14 +0000

    fix: revert

--- src/freshland/index.ts
+++ src/freshland/index.ts
@@ -25,7 +25,9 @@ export class Freshland {
         throw new Error(
           '[DESTINATION_NOT_EMPTY] Destination isn\'t empty, aborting the process. (use "<FreshBuilder>.setForce(true)" or provide "--force" flag to bypass)'
         );
-      }
+      } //else {
+      //   logger.warn("Destination directory isn't empty. Skipping (force mode)");
+      // }
 
       switch (builderData.mode) {
         case 'tar':
