155152f32c40897cc9db9513875de8e1f4c513c8
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2025-07-05 11:11:41 +0000

    fix: add missing branches for semantic-release

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  // branches: ['main', 'beta'],
+  branches: ['main', 'beta'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
