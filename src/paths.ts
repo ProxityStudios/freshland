3985638c2aa4ccc9f8fc9e6a81a0106dc6c48fb5
Alexinhans <103999185+Alexinhans@users.noreply.github.com>
2025-07-13 21:36:43 +0000

    fix: invalid paths

--- src/paths.ts
+++ src/paths.ts
@@ -1,4 +1,4 @@
 import path from 'node:path';
 
-export const TemplatesRAWDataPath = path.resolve(process.cwd(), '.data/templates.json');
-export const ProxyConfigRAWDataPath = path.resolve(process.cwd(), '.data/proxy.json');
+export const TemplatesRAWDataPath = path.resolve('.data/templates.json');
+export const ProxyConfigRAWDataPath = path.resolve('.data/proxy.json');
